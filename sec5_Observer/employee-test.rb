9c6b7ac507c6a96db05c90e5aa8b9ad0b70aa25c
yoshiaki rikitake <yoshiaki@sendai-nct.ac.jp>
2013-04-01 10:26:41 +0900

    add Payroll: hard coding version

--- sec5_Observer/employee-test.rb
+++ sec5_Observer/employee-test.rb
@@ -1,6 +1,7 @@
 require 'employee'
 
-fred = Employee.new("Fred Flintstone", "Crane Operator", 30000.0)
+payroll = Payroll.new
+fred = Employee.new("Fred Flintstone", "Crane Operator", 30000.0, payroll)
 
 fred.salary = 35000.0
 
--- sec5_Observer/employee.rb
+++ sec5_Observer/employee.rb
@@ -1,10 +1,18 @@
+require 'payroll'
+
 class Employee
 	attr_reader :name
-	attr_accessor :title, :salary
+	attr_reader :title, :salary
 
-	def initialize(name, title, salary)
+	def initialize(name, title, salary, payroll)
 		@name = name
 		@title = title
 		@salary = salary
+		@payroll = payroll
+	end
+	
+	def salary=(new_salary)
+		@salary = new_salary
+		@payroll.update(self)
 	end
 end
