ad9d2d3ef91aee037079b9ab7dc2be9f83febac6
yoshiaki rikitake <yoshiaki@sendai-nct.ac.jp>
2013-04-01 10:38:09 +0900

    applied Observer pattern for Employee, Payroll, TaxMan

--- sec5_Observer/employee-test.rb
+++ sec5_Observer/employee-test.rb
@@ -1,7 +1,14 @@
 require 'employee'
+require 'payroll'
+require 'taxman'
+
+fred = Employee.new("Fred Flintstone", "Crane Operator", 30000.0)
 
 payroll = Payroll.new
-fred = Employee.new("Fred Flintstone", "Crane Operator", 30000.0, payroll)
+fred.add_observer(payroll)
 
-fred.salary = 35000.0
+taxman = TaxMan.new
+fred.add_observer(taxman)
+
+fred.salary = 35000.0 
 
