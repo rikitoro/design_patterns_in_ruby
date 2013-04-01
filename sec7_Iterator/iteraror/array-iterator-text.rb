bef187bffafca123664083d0f640f2bf5bba6e7e
yoshiaki rikitake <yoshiaki@sendai-nct.ac.jp>
2013-04-01 15:39:24 +0900

    first commit for sec7

---
+++ sec7_Iterator/iteraror/array-iterator-text.rb
@@ -0,0 +1,14 @@
+require 'array-iterator'
+
+array = ['red', 'green', 'blue']
+
+i = ArrayIterator.new(array)
+while i.has_next?
+	puts("item: #{i.next_item}")
+end
+
+i = ArrayIterator.new('abc')
+while i.has_next?
+	puts("item: #{i.next_item.chr}")
+end
+
---
+++ sec7_Iterator/iteraror/implicite-iterator-text.rb
@@ -0,0 +1,4 @@
+require 'implicite-iterator'
+
+a = [10, 20, 30]
+for_each_element(a) {|element| puts("The element is #{element}")}
