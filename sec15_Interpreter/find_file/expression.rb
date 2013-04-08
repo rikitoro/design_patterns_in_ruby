524c82cd30db5ad2415362ea34c15ffca1c41aa6
yoshiaki rikitake <yoshiaki@sendai-nct.ac.jp>
2013-04-08 22:02:24 +0900

    add Not

--- sec15_Interpreter/find_file/expression.rb
+++ sec15_Interpreter/find_file/expression.rb
@@ -34,3 +34,57 @@ class FileName < Expression
 end
 
 
+class Bigger < Expression
+	def initialize(size)
+		@size = size
+	end
+	
+	def evaluate(dir)
+		results = []
+		Find.find(dir) do |p|
+			next unless File.file?(p)
+			results << p if (File.size(p) > @size)
+		end
+		results
+	end	
+end
+
+
+class Writable < Expression
+	def evaluate(dir)
+		results = []
+		Find.find(dir) do |p|
+			next unless File.file?(p)
+			results << p if (File.writable?(p))
+		end
+		results
+	end
+end
+
+
+class Not < Expression
+	def initialize(expression)
+		@expression = expression
+	end
+	
+	def evaluate(dir)
+		All.new.evaluate(dir) - @expression.evaluate(dir)
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sec15_Interpreter/find_file/test.rb
+++ sec15_Interpreter/find_file/test.rb
@@ -4,6 +4,18 @@ expr_all = All.new
 files = expr_all.evaluate('test_dir')
 p files
 
-expr_mp3 = FileName.new('*.txt')
+expr_mp3 = FileName.new('*.mp3')
 mp3s = expr_mp3.evaluate('test_dir')
 p mp3s
+
+expr_jpg = FileName.new('*.jpg')
+jpgs = expr_jpg.evaluate('test_dir')
+p jpgs
+
+expr_bigger = Bigger.new(50)
+bigger = expr_bigger.evaluate('test_dir')
+p bigger
+
+small_expr = Not.new(Bigger.new(50))
+small_files = small_expr.evaluate('test_dir')
+p small_files
