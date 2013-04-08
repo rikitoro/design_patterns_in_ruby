a9c4632fa71cd26bee3ab61ca21da95109189837
yoshiaki rikitake <yoshiaki@sendai-nct.ac.jp>
2013-04-08 22:15:11 +0900

    add And

--- sec15_Interpreter/find_file/test.rb
+++ sec15_Interpreter/find_file/test.rb
@@ -19,3 +19,15 @@ p bigger
 small_expr = Not.new(Bigger.new(50))
 small_files = small_expr.evaluate('test_dir')
 p small_files
+
+expr_mp3_or_jpg = Or.new(expr_mp3, expr_jpg)
+mp3_or_jpgs = expr_mp3_or_jpg.evaluate('test_dir')
+p mp3_or_jpgs
+
+expr_mp3_and_bigger = And.new(expr_mp3, expr_bigger)
+mp3_and_biggers = expr_mp3_and_bigger.evaluate('test_dir')
+p mp3_and_biggers
+
+expr_complex = And.new(Not.new(FileName.new('*.jpg')), Not.new(Bigger.new(50)))
+files = expr_complex.evaluate('test_dir')
+p files
